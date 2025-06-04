a4a754b591e9dd4b37a917355a0cbfc21c2c895c
Jean Conti <114805482+JeanConti@users.noreply.github.com>
2025-06-04 18:46:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,71 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const tacheInput = document.getElementById('tacheInput')
-  const addTacheBtn = document.getElementById('addTacheBtn')
-  const listeTaches = document.getElementById('listeTaches')
-  const heure = document.getElementById('heure')
-
-  // Function pour avoir l'heure et la date de la tâche
-  const getCurrentDateTime = () => {
-    const now = new Date()
-    const date = now.toLocaleDateString()
-    const time = now.toLocaleTimeString()
-    return `${date} ${time}`
-  }
-
-  // Actualiser l'horloge digital
-  const actuHeure = () => {
-    const now = new Date()
-    const timeString = now.toLocaleTimeString()
-    heure.textContent = timeString
-  }
-
-  // Actualiser l'horloge a chaque segonde
-  setInterval(actuHeure, 1000)
-  actuHeure()
-
-  // Funtion pour ajouter une tâche
-  const addTache = () => {
-  const textTache = tacheInput.value.trim()
-  if (textTache !== '') {
-    const li = document.createElement('li')
-    const dateTime = getCurrentDateTime()
-    li.innerHTML =
-      `
-      <span>${textTache}</span>
-      <span class="datetime">${dateTime}</span>
-      <button>Delete</button>
-      `
-    listeTaches.appendChild(li)
-    tacheInput.value = ''
-
-    // Marqué la tache comme faite
-    li.querySelector('span').addEventListener('click', () => {
-      li.classList.toggle('Tache faite')
-    })
-
-    // Eliminé une tâche
-    li.querySelector('button').addEventListener('click', () => {
-      listeTaches.removeChild(li)
-    })
-  }
-}
-
-  // Événement pour ajouter une tâche en cliquant sur le bouton
-  addTacheBtn.addEventListener('click', addTache)
-
-  // Événement en pressionant ENTER pour ajouter
-  tacheInput.addEventListener('keydown', (e) => {
-    if (e.key == 'Enter') {
-      addTache()
-    }
-  })
-})
-
-
-
-
-
-/* 
+ 
 document.addEventListener('DOMContentLoaded', () => {
     const taskInput = document.getElementById('taskInput');
     const addTaskBtn = document.getElementById('addTaskBtn');
@@ -115,4 +48,3 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
- */
